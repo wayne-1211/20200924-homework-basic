9763179918f742f20245775e6947c931efa48d59
林柏崴 <wayne20051211@gmail.com>
2020-09-30 20:05:40 +0800

    homework_submit

---
+++ 755446/expense_app.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+    cout << "height: ";
+    cin >> height;
+
+    for (int i = height; i > 0; i--) {
+      for (int j = i; j > 0; j--) {
+        cout << " ";
+      }
+      for (int k = height - i + 1; k > 0; k--) {
+        cout << "#";
+      }
+      cout << "\n";
+    }
+}
---
+++ 755446/mario.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int count, total, temporarily = 0;
+  
+  cout << "請問有幾筆資料: ";
+  cin >> count;
+
+  for (int i = 0; i < count; i++) {
+      temporarily = 0;
+      cout << "請輸入第" << i+1 << "筆存款:" ;
+      cin >> temporarily;
+      total = total + temporarily;
+  }
+  cout << "一共是 " << total << "元";
+}
