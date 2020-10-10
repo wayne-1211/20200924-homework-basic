c922e165d8d1c37d8a661cdf1d8e6eece5302b24
林柏崴 <wayne20051211@gmail.com>
2020-10-10 11:10:15 +0800

    exam questions

---
+++ 755446/Isosceles_triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+    cout << "height: ";
+    cin >> height;
+    for (int i=1;i<=height;i++) {
+        for(int j=height-i;j>0;j--) {
+            cout<<" ";
+        }
+        for(int k=1;k<=2*i-1;k++){
+            cout<<"*";
+        }
+    cout<<"\n";
+    }
+}
