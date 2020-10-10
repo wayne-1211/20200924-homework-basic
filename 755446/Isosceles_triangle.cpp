16409ea5a6738c8319b04e43c7df02b3e2d55765
林柏崴 <65344622+wayne-1211@users.noreply.github.com>
2020-10-10 11:13:02 +0800

    Update Isosceles_triangle.cpp

--- 755446/Isosceles_triangle.cpp
+++ 755446/Isosceles_triangle.cpp
@@ -5,13 +5,13 @@ int main() {
     int height;
     cout << "height: ";
     cin >> height;
-    for (int i=1;i<=height;i++) {
-        for(int j=height-i;j>0;j--) {
-            cout<<" ";
+    for (int i = 1; i <= height; i++) {
+        for (int j = height-i; j > 0; j--) {
+            cout << " ";
         }
-        for(int k=1;k<=2*i-1;k++){
-            cout<<"*";
+        for (int k = 1; k <= 2 * i-1; k++){
+            cout << "*";
         }
-    cout<<"\n";
+    cout << "\n";
     }
 }
